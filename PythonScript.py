422757b6d85e709ff12d57beb90dffe370558aac
Chaddnius <90437367+Chaddnius@users.noreply.github.com>
2023-09-18 22:27:05 -0500

    Fixed the issue

--- PythonScript.py
+++ PythonScript.py
@@ -45,12 +45,13 @@ for log_entry in log_data:
     if "[" in log_entry:
         date_str = log_entry.split("[")[1].split(" ")[0]
     log_date = datetime.strptime(date_str, "%d/%b/%Y:%H:%M:%S")
-
-    if start_date <= log_date <= end_date:
+    if ("- -" not in log_entry):
+        total_requests_in_6_months += 0
+    elif (start_date <= log_date <= end_date):
         total_requests_in_6_months += 1  # Increment the count if it falls within the period
 
 # Decrement the count after the loop
-total_requests_in_6_months -= 1
+#total_requests_in_6_months -= 1
 
 # Now 'total_requests_in_6_months' contains the total requests made in the 6 months
 print(f"Total requests made in the last 6 months: {total_requests_in_6_months}")
